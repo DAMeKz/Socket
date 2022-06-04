50f83eaa4e37ff7ba69936afb640faef3a33c93a
DAMeKz <hwanseo6417@naver.com>
2022-06-05 02:47:57 +0900

    _

--- TCP_c/client.c
+++ TCP_c/client.c
@@ -1,49 +1,40 @@
-#include <sys/un.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 
 void error_handling(char *message);
 
-int main(int argc, char *argv[]){
-    int server, client;
-
+int main(int argc, char* argv[]){
+    int client;
     struct sockaddr_in serv_addr;
-    struct sockaddr_in clnt_addr;
-    socklen_t clnt_addr_size;
+    char message[1024]={0x00,};
 
-    server = socket(PF_INET, SOCK_STREAM,0);
-    if(server == -1){
+    client = socket(PF_INET,SOCK_STREAM,0);
+    if(client==-1){
         error_handling("socket error");
     }
 
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family=AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(atoi(argv[1]));
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+    serv_addr.sin_port=htons(atoi(argv[2]));
 
-    if(bind(server,(struct sockaddr *)&serv_addr, sizeof(serv_addr))==1){
-        error_handling("bind error");
+    if(connect(client, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1){
+        error_handling("connect error");
     }
 
-    if(listen(server,5)==-1){
-        error_handling("listen error");
+    if(read(client, message,sizeof(message)-1)==-1){
+        error_handling("read_error");
     }
 
-    clnt_addr_size = sizeof(clnt_addr);
-    client = accept(server, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-    if(client==-1){
-        error_handling("accept error");
-    }
-
-    char msg[] = "Hello this is server!\n";
-    write(client, msg, sizeof(msg));
+    printf("Message from server: %s\n",message);
 
     close(client);
-    close(server);
+    return 0;
 }
 
 void error_handling(char *message){
--- TCP_c/server.c
+++ TCP_c/server.c
@@ -1,41 +1,49 @@
-#include <sys/socket.h>
 #include <sys/un.h>
+#include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#define PORTNUM 9000
+#include <string.h>
 
 void error_handling(char *message);
 
-int main(int argc, char* argv[]){
-    int client;
-    struct sockaddr_in serv_addr;
-    char message[1024]={0x00,};
+int main(int argc, char *argv[]){
+    int server, client;
 
-    client = socket(PF_INET,SOCK_STREAM,0);
-    if(client==-1){
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size;
+
+    server = socket(PF_INET, SOCK_STREAM,0);
+    if(server == -1){
         error_handling("socket error");
     }
 
-    memset(&serv_addr,0,sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-    serv_addr.sin_port=htons(atoi(argv[2]));
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
 
-    if(connect(client, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1){
-        error_handling("connect error");
+    if(bind(server,(struct sockaddr *)&serv_addr, sizeof(serv_addr))==1){
+        error_handling("bind error");
     }
 
-    if(read(client, message,sizeof(message)-1)==-1){
-        error_handling("read_error");
+    if(listen(server,5)==-1){
+        error_handling("listen error");
     }
 
-    printf("Message from server: %s\n",message);
+    clnt_addr_size = sizeof(clnt_addr);
+    client = accept(server, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+    if(client==-1){
+        error_handling("accept error");
+    }
+
+    char msg[] = "Hello this is server!\n";
+    write(client, msg, sizeof(msg));
 
     close(client);
-    return 0;
+    close(server);
 }
 
 void error_handling(char *message){
